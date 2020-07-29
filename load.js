9695376c6e571534092651711ca429644aa70307
dmatlock96 <56280214+dmatlock96@users.noreply.github.com>
2020-07-29 15:52:46 -0700

    Update load.js

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
-const yearStart = 2000;
-const yearEnd = 2020;
+const yearStart = 2005;
+const yearEnd = 2015;
 const totalNoOfCountriesToLoad = 400;
 
 const margin = {top: 20, right: 120, bottom: 50, left: 50},
