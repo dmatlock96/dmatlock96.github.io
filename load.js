0a8d56ac649e178e91b03460977032a6a94558bf
dmatlock96 <56280214+dmatlock96@users.noreply.github.com>
2020-07-29 19:42:28 -0700

    Update load.js

--- load.js
+++ load.js
@@ -1,15 +1,18 @@
 const yearStart = 2005;
 const yearEnd = 2020;
 const totalNoOfCountriesToLoad = 400;
+
 const margin = {top: 20, right: 120, bottom: 50, left: 50},
     svgWidth = 900,
     svgHeight = 600,
     width = svgWidth - margin.left - margin.right,
     height = svgHeight - margin.top - margin.bottom;
+
 var parseTime = d3.timeParse("%Y");
 var formatValue = d3.format(",");
 var floatFormatValue = d3.format(".3n");
 // WDI call type 
+
 const type = {
     TOTAL: 0,
     MAILE: 1,
@@ -72,17 +75,17 @@ $("#to_step3").click(function() {
      draw("RUS", false, 2);
  })
 
- $("#to_step5").click(function() {
-     //d3.selectAll("path").remove();
-     innerChart.selectAll("g").remove();
-     hide('#step4');
- $("#to_step5").click(function() {
-     draw("WLD", true, 0);
-     draw("USA", true, 0);
-     draw("CHN", true, 0);
-     draw("RUS", true, 0);
-
- })
+$("#to_step5").click(function() {
+    //d3.selectAll("path").remove();
+    innerChart.selectAll("g").remove();
+    hide('#step4');
+    loadCountries(addCountriesList);
+    show('#step5');
+    draw("WLD", true, 0);
+    draw("USA", true, 0);
+    draw("CHN", true, 0);
+    draw("RUS", true, 0);
+})
 
 $("#startover").click(function() {
     innerChart.selectAll("g").remove();
@@ -117,4 +120,169 @@ function loadTotalEmploymentByCountryCode(countryCode, callback){
 function loadFemaleEmploymentByCountryCode(countryCode, callback){
     d3.json("https://api.worldbank.org/v2/country/" + countryCode + "/indicator/SL.EMP.WORK.FE.ZS?format=json&per_page=60&date=" + yearStart + ":" + yearEnd)
         .then(callback);
+}
+function loadMaleEmploymentByCountryCode(countryCode, callback){
+    d3.json("https://api.worldbank.org/v2/country/" + countryCode + "/indicator/SL.EMP.WORK.MA.ZS?format=json&per_page=60&date=" + yearStart + ":" + yearEnd)
+        .then(callback);
+}
+/**
+ * 
+ * @param {*} countryCode 3-digit country code
+ * @param {*} type "male", "female", "total" (male+female)
+ * @param {*} callback callback function 
+ */
+function loadEmploymentByCountryCode(countryCode, type, callback){
+    if (type == "male"){
+        loadMaleEmploymentByCountryCode(countryCode, callback);
+    }
+    else if (type == "female"){
+        loadFemaleEmploymentByCountryCode(countryCode, callback);
+    }
+    else if (type == "total"){
+        loadTotalEmploymentByCountryCode(countryCode, callback);
+    }
+    else {
+        console.error("no proper type", type);
+    }
+}
+// Only for debugging purpose, provide this function as callback for those API calls to see the loaded data
+function debug(d){
+    console.log("DEBUG) data loaded:", d);
+}
+/**
+ * callback function
+ * @param {*} countryCode 3-digit country code to query, "WLD" is for the world.
+ * @param {*} countrylabel true of false for drawing line tooltip 
+ * @param {*} type type constant, 0: total, 1: male, 2: female
+ */
+function draw(countryCode, countrylabel, type) {
+    console.log("country in draw():", countryCode);
+    if (type == 0){
+        loadEmploymentByCountryCode(countryCode, "total", drawChart(countryCode, countrylabel, "red"));
+    }
+    else if (type == 1){
+        loadEmploymentByCountryCode(countryCode, "male", drawChart(countryCode, countrylabel, "green"));
+    }
+    else if (type == 2){
+        loadEmploymentByCountryCode(countryCode, "female", drawChart(countryCode, countrylabel, "blue"));
+    }
+    else {
+        console.log("error in draw(), type:", type);
+    }
+}
+/**
+ * callback function for d3.json()
+ * @param {*} countryCode 3-digit country code to draw a linechart and also for label.
+ * @param {*} countrylabel true of false for drawing line tooltip 
+ * @param {*} color color string to to draw line chart. e.g, "red", "black", etc.
+ */
+function drawChart(countryCode, countrylabel, color){
+    console.log("Color parameter received in drawChart", color);
+    // done this way to take extra parameter and pass it to the callback.
+    return function(data){
+        //console.log("data[0] in draw():", data[0]);
+        console.log("data[1] in draw():", data[1]);
+        if (data == null || data[1] == null){
+            $('.alert').show();
+            return;
+        }
+        //  clean up everything before drawing a new chart
+        // d3.select("body").selectAll("svg > *").remove();
+        xScale.domain(d3.extent(data[1], function(d) { return d.date; }));
+        yScale.domain([0, 100]);
+        // Add the X Axis
+        console.log("add x axis");
+        innerChart
+            .append('g')
+            .attr('transform', "translate(0," + height + ")")
+            .call(xAxis);
+        innerChart
+            .append("text")             
+            .attr("transform",
+                "translate(" + (width/2) + " ," + 
+                                (height + margin.top + 20) + ")")
+            .style("text-anchor", "middle")
+            .text("year");
+        console.log("add y axis");
+        // Add the Y Axis
+        innerChart
+            .append('g')
+            .call(yAxis)
+            .attr("y", 6);
+        innerChart
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("percentage");
+        console.log("draw data");
+        /* Initialize tooltip for datapoint */
+        tip = d3.tip().attr('class', 'd3-tip').offset([-5, 5]).html(function(d) {
+            return "<span style='color:" + color + "'>" + countryCode + " " + floatFormatValue(d.value) "</span>";
+        });   
+        var path = innerChart.append("g").append("path")
+        .attr("width", width).attr("height",height)
+        .datum(data[1].map( (d, i) => {
+            console.log("path : date", d.date, "value", d.value);
+            return {
+                date : d.date,
+                value : d.value
+            };
+        }
+        ))
+        .attr("class", "line")
+        .attr("d", valueline)
+        .style("stroke", color);        
+        // datapoint tooltip
+        innerChart.append("g").selectAll(".dot")
+            .attr("width", width).attr("height",height)
+            .data(data[1])
+            .enter()
+            .append("circle") // Uses the enter().append() method
+            .attr("class", "dot") // Assign a class for styling
+            .attr("cx", function(d) { return xScale(d.date) })
+            .attr("cy", function(d) { return yScale(d.value) })
+            .attr("r", 3)
+            .call(tip)
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide);
+        if (countrylabel == true){
+            innerChart.selectAll().data(data[1]).enter().append("g").append("text")
+            .attr("transform", "translate(" + (width - 20) + "," + yScale(data[1][data[1].length - 1].value) + ")")
+            .attr("dy", ".15em")
+            .attr("text-anchor", "start")
+            .style("fill", color)
+            .text(countryCode);
+        }
+    }
+}
+// callback function
+function addCountriesList(data, i){
+    d3.select("body")
+        .select("#country_select_container")
+        .append("select")
+        .attr("id", "country")
+        .selectAll("options")
+        .data(data[1])
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d.id; })
+        .text(function (d, i){return d.name;});
+    d3.select("body").select("#country_select_container").select("select").on("change", function(){
+        console.log(d3.select(this).property('value'));
+        draw(
+            d3.select(this).property('value'), 
+            true,
+            d3.select('input[name=type]:checked').node().value
+        );
+    });
+}
+// utility functions
+function show(step){
+    $(step).show();
+}
+function hide(step){
+    $(step).hide();
 }
