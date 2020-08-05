434d07d5b8b454861ac59809a4bdc5e7aafebd0e
dmatlock96 <56280214+dmatlock96@users.noreply.github.com>
2020-08-05 15:30:41 -0700

    Update load.js

--- load.js
+++ load.js
@@ -257,7 +257,7 @@ function drawChart(countryCode, countrylabel, color){
 
         /* Initialize tooltip for datapoint */
         tip = d3.tip().attr('class', 'd3-tip').offset([-5, 5]).html(function(d) {
-            return "<strong style='color:" + color + ";background:none'>" + countryCode + " " + floatFormatValue(d.value)  + "</strong>";
+            return "<strong style='color:" + color + ";background-color:none'>" + countryCode + " " + floatFormatValue(d.value)  + "</strong>";
         });   
 
         var path = innerChart.append("g").append("path")
